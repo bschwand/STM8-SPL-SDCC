2186221f3ecf8c50ec0f70b342994b91b9036150
Bruno Schwander <bruno.schwander@gmail.com>
2018-02-09 07:29:58 +0100

    add missing interrupt handler declaration, or they won-t work at all

--- Project/STM8S_StdPeriph_Template/main.c
+++ Project/STM8S_StdPeriph_Template/main.c
@@ -28,6 +28,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm8s.h"
+#include "stm8s_it.h"    // SDCC requires ISR declaration to be included here
 
 /* Private defines -----------------------------------------------------------*/
 /* Private function prototypes -----------------------------------------------*/
